10049f092ae1a4b02c31d3089cd48304b835970e
Carlos Solorzano <csolorzanoflores@gmail.com>
2022-05-30 18:27:15 -0600

    final

--- app/src/main/java/net/csolorzano/clase3/MainActivity.kt
+++ app/src/main/java/net/csolorzano/clase3/MainActivity.kt
@@ -2,10 +2,31 @@ package net.csolorzano.clase3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG,"Actividad onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"Actividad onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"Actividad onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"Actividad onResume")
+    }
+
+    companion object{
+        public const val TAG = "DispMov"
     }
 }
